12bcd2f0993c32063321f4dc5a274c68ee4a5517
Jivan Amara <Development@JivanAmara.net>
2016-08-09 18:25:53 -0700

    Bumped version to 0.1.3. Tagged as v0.1.3.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ data_files = audio_samples + fixtures
 
 setup(
     name="syllable-samples",
-    version="0.1.2",
+    version="0.1.3",
     author="Jivan Amara",
     author_email="Development@JivanAmara.net",
     packages=['syllable_samples', 'syllable_samples.migrations'],
