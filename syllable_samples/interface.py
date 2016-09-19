4d0c3b25c4a50d80c8d205dd0007363598a9ab0e
Jivan Amara <Development@JivanAmara.net>
2016-09-19 16:39:29 -0700

    Added hanzi list to get_random_syllable() function.

--- syllable_samples/interface.py
+++ syllable_samples/interface.py
@@ -5,11 +5,12 @@ Created on Jul 25, 2016
 '''
 from syllable_samples.models import SyllableSample
 import random
+import json
 
 def get_random_sample():
     nsamples = SyllableSample.objects.order_by('id').count()
     s_idx = random.randint(0, nsamples - 1)
     s = SyllableSample.objects.order_by('id')[s_idx]
-
-    ret = (s.sound, s.tone, s.display, s.path)
+    hanzi = json.loads(s.hanzi_examples_json_string)
+    ret = (s.sound, s.tone, s.display, s.path, hanzi)
     return ret
